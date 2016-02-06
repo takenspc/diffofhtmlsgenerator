11aa1a1db8d6af8ede0271ca896cc068f9bdafe1
Takeshi Kurosawa <taken.spc@gmail.com>
2016-02-07 00:03:46 +0900

    Add firebase to the dependencies

--- typings/bundle.d.ts
+++ typings/bundle.d.ts
@@ -6,3 +6,4 @@
 /// <reference path="diff/diff.d.ts" />
 /// <reference path="moment/moment.d.ts" />
 /// <reference path="moment/moment-node.d.ts" />
+/// <reference path="firebase/firebase.d.ts" />
