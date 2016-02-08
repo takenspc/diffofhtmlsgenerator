3bbdac68cda6d100127a562481dc883395354782
Takeshi Kurosawa <taken.spc@gmail.com>
2016-02-09 01:20:50 +0900

    Remove global.gc at all

--- diff/htmlDiff.ts
+++ diff/htmlDiff.ts
@@ -6,7 +6,5 @@ import { diffSection } from './htmlDiffChild';
 export async function computeHTMLDiff(sections: DiffEntry[]): Promise<void> {
     for (const section of nextLeafDiffEntry(sections)) {
         await diffSection(section);
-        // XXX MAKE HEROKU HAPPY
-        global.gc();
     }
 }
