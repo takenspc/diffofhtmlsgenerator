870d24212e3a96115b1615a9ea6fdb58542f9476
Takeshi Kurosawa <taken.spc@gmail.com>
2016-02-14 10:36:44 +0900

    diff: Reorder Calling scripts

--- diff/index.ts
+++ diff/index.ts
@@ -1,4 +1,5 @@
 'use strict';
+import * as assert from 'assert';
 import * as path from 'path';
 import { writeFile, readFile, mkdirp } from '../utils';
 import { JSONEntry, readJSONEntry } from '../jsonEntry';
@@ -6,40 +7,71 @@ import { DiffEntry, writeDiffEntry } from '../diffEntry';
 import { computeJSONDiff } from './jsonDiff';
 import { computeHTMLDiff } from './htmlDiff';
 
-//
-// XXX Reoder W3C 'Links' of 'The elements of HTML'
-//
-function reorderW3CSemantics(sections: JSONEntry[]): JSONEntry[] {
+/**
+ * move targetId section before referenceId
+ */
+function moveEntryBefore(entries: JSONEntry[], targetId: string, referenceId: string): JSONEntry[] {
     const tmp: JSONEntry[] = [];
-    let links: JSONEntry;
-    for (const section of sections) {
-        if (section.id === 'links') {
-            links = section;
-        } else {
-            tmp.push(section);
+    let target: JSONEntry = null;
+    let reference: JSONEntry = null;
+    for (const entry of entries) {
+        const id = entry.id;
+        if (entry.id === targetId) {
+            target = entry;
+            continue;
         }
+
+        if (entry.id === referenceId) {
+            reference = entry;
+        }
+
+        tmp.push(entry);
     }
+    assert(target, `There must be #${targetId} section`);
+    assert(reference, `There must be #${referenceId} section`);
 
     const reorderd: JSONEntry[]= [];    
-    for (const section of tmp) {
-        if (section.id === 'edits') {
-            reorderd.push(links);
+    for (const entry of tmp) {
+        if (entry === reference) {
+            reorderd.push(target);
         }
 
-        reorderd.push(section);
+        reorderd.push(entry);
     }
 
     return reorderd;
 }
 
-function reoderW3C(entries: JSONEntry[]): JSONEntry[] {
+function getEntryById(entries: JSONEntry[], id: string): JSONEntry {
     for (const entry of entries) {
-        // 4 The elements of HTML
-        if (entry.id === 'semantics') {
-            entry.sections = reorderW3CSemantics(entry.sections);
+        if (entry.id === id) {
+            return entry;
         }
     }
-    return entries;
+
+    let child = null;
+    for (const entry of entries) {
+        child = getEntryById(entry.sections, id);
+        if (child) {
+            return child;
+        }
+    }
+
+    return null;
+}
+
+function reoderW3C(entries: JSONEntry[]): void {
+    // 4 The elements of HTML
+    const semantics = getEntryById(entries, 'semantics');
+    assert(semantics, 'w3c entries must have #semantics section');
+    // Move 'Links' before 'Edits'
+    semantics.sections = moveEntryBefore(semantics.sections, 'links', 'edits');
+    
+    // 7 Web application APIs
+    const processingModel = getEntryById(entries, 'scripting-processing-model');
+    assert(processingModel, 'w3c entries must have #scripting-processing-model section');
+    // Move 'Calling scripts' before 'Killing scripts'
+    processingModel.sections = moveEntryBefore(processingModel.sections, 'calling-scripts', 'killing-scripts');
 }
 
 
@@ -54,7 +86,11 @@ export async function diff() {
     ]);
 
     const whatwg = jsonEntries[0];
-    const w3c = reoderW3C(jsonEntries[1]);
+    const w3c = jsonEntries[1];
+
+    // reorder 
+    reoderW3C(w3c);
+
     const diffEntries = computeJSONDiff(whatwg, w3c);
 
     const outDir = path.join(__dirname, 'data');
