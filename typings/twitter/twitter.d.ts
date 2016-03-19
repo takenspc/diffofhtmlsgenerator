8ecb38cf7c7622e896cd2e35f870097053493dbe
Takeshi Kurosawa <taken.spc@gmail.com>
2016-03-19 17:08:46 +0900

    Add twitter module

---
+++ typings/twitter/twitter.d.ts
@@ -0,0 +1,27 @@
+﻿declare module 'twitter' {
+    interface Callback {
+        (err: Error, tweet: any, response: any): any;
+    }
+    
+    interface Tweet {
+        status: string
+    }
+    
+    interface Twitter_Option {
+        consumer_key: string,
+        consumer_secret: string,
+        access_token_key: string,
+        access_token_secret: string,
+    }
+    
+    interface Twitter_Static {
+        new(option: Twitter_Option): Twitter_Instance
+    }
+    
+    interface Twitter_Instance {
+        post(path: string, Tweet, Callback): void;
+    }
+
+    var Twitter: Twitter_Static;
+	export = Twitter;
+}
